9bddcf09778f74394f0474c4724606447b8b4d34
Sarah Martel <57870970+sarahmarie23@users.noreply.github.com>
2024-05-10 11:24:17 -0700

    Lab 6 and 7 upload

---
+++ Labs/Lab7.java
@@ -0,0 +1,26 @@
+/**
+ * Write a description of class Lab7 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lab7 {
+    public static void main(String[] args) {
+        // TODO: Implement user input
+        
+        // TODO: Call each of your methods
+          
+    }  
+
+
+    /**
+     * Write a description of the method here
+     * 
+     * @param input
+     */
+    public static void capitalizeFirstLetters(String input) {
+        // TODO: Parse input for words and capitalize each one.
+    }
+
+    // TODO: Implement the rest of the methods from the lab instructions
+}  
